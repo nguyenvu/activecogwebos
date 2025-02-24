1af60a18bfede148ceef11742ef5775f2daa5067
Nguyen Vu <nguyenvu@live.com>
2025-02-24 08:47:55 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,36 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        // specifying /(.*) in the source sets security headers on all routes
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; font-src 'self' https://fonts.googleapis.com; img-src 'self' *.api.radio-browser.info; script-src 'self'",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(); battery=(); geolocation=(); microphone=()",
+          },
+        ],
+      },
+    ];
+  },
 };
-
-export default nextConfig;
