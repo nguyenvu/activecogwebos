685c3cdcf49b014fd70e25ee6bb31b7d9c7d9f84
Nguyen Vu <nguyenvu@live.com>
2025-02-24 08:44:38 +0100

    Update middleware.js

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -1,3 +1,32 @@
+export function middleware(request: NextRequest) {
+    const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+
+    const csp = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
+    `;
+     const cspValue = csp.replace(/\s{2,}/g, " ").trim();
+
+    const reqHeaders = new Headers(request.headers);
+
+    reqHeaders.set("X-Nonce", nonce);
+    reqHeaders.set("Content-Security-Policy", cspValue);
+
+    const response= NextResponse.next({
+        request: {
+             headers: reqHeaders
+        }
+    });
+
+    response.headers.set("Content-Security-Policy", cspValue);
+
+    return response;
+}
+
+
+// We also don't include csp header when retrieving static assets.
+
 export const config = {
   matcher: [
     /*
@@ -5,31 +34,14 @@ export const config = {
      * - api (API routes)
      * - _next/static (static files)
      * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     * - favicon.ico (favicon file)
      */
     {
-      source:
-        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
       missing: [
         { type: 'header', key: 'next-router-prefetch' },
         { type: 'header', key: 'purpose', value: 'prefetch' },
       ],
-    },
- 
-    {
-      source:
-        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
-      has: [
-        { type: 'header', key: 'next-router-prefetch' },
-        { type: 'header', key: 'purpose', value: 'prefetch' },
-      ],
-    },
- 
-    {
-      source:
-        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
-      has: [{ type: 'header', key: 'x-present' }],
-      missing: [{ type: 'header', key: 'x-missing', value: 'prefetch' }],
     },
   ],
 }
